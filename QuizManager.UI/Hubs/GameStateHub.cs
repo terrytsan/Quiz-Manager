c2ea3aeb1cd9ab488175461dd47c5a7409573701
terrytsan <terrytsan@hotmail.com>
2020-11-25 16:22:05 +0000

    Update Quiz Details Page
    - Added ability to advance to the next question on the quiz details page.
    - Added SignalR gameState hub.

---
+++ QuizManager.UI/Hubs/GameStateHub.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNet.SignalR;
+using QuizManager.Data.Factories;
+
+namespace QuizManager.UI.Hubs
+{
+    public class GameStateHub : Hub
+    {
+        public void NextQuestion(int quizId)
+        {
+            var gameStateRepo = GameStateRepositoryFactory.GetRepository();
+
+            // Get the next question number
+            var nextQuestion = gameStateRepo.GetNextQuestionForQuiz(quizId);
+
+            if (nextQuestion != null)
+            {
+                // Update the database gameState
+                gameStateRepo.UpdateCurrentQuestionForQuiz(quizId, nextQuestion.Id);
+
+                // Broadcast updated gameState to clients
+                Clients.All.updateCurrentQuestionText(nextQuestion);
+            }
+            else
+            {
+                // If no more questions, show alert
+                Clients.All.showEndOfQuizAlert();
+            }
+        }
+    }
+}
